4f353944c63e77f874a15c588ac367f7b14d4e6d
HurricanKai <herrucan.schmitt@gmail.com>
2019-01-29 13:58:12 +0100

    Closes #219

---
+++ Protocol.Latest/Packets/Play/KeepAliveServerbound.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Server;
+
+namespace Protocol.Latest.Packets
+{
+    public sealed class KeepAliveServerbound : IPacket
+    {
+        public int Id => 0x0E;
+
+        public ConnectionState DesiredState => ConnectionState.Play;
+
+        public bool Clientbound => false;
+
+        public long KeepAliveID { get; set; }
+
+        public async Task Read(Stream stream, RemoteClient client)
+        {
+            KeepAliveID = await NetworkUtils.ReadLong(stream);
+        }
+
+        public Task Write(Stream stream, RemoteClient client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
